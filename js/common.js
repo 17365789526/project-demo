b4835cd69e663aad808816eff0c24704f09c527a
jepson <1528238450@qq.com>
2023-02-12 01:55:47 +0800

    轻提示测试完成

--- js/common.js
+++ js/common.js
@@ -5,3 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+
+// 封装轻提示函数 tip
+const toastBox = document.querySelector('#myToast')
+  const toast = new bootstrap.Toast(toastBox, {
+  animation: true,    // 开启过度动画
+  autohide: true,     // 开启自动隐藏
+ delay: 3000         // 3000ms后自动隐藏
+})
+
+const tip = (msg) => {
+  toastBox.querySelector('.toast-body').innerHTML = msg
+  toast.show()
+}
