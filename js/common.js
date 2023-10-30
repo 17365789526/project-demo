381eb2b2c98326bd0bb1152323ca229fd086a0db
milibing <1528238450@qq.com>
2023-10-30 23:27:46 +0800

    抽取轻提示函数

--- js/common.js
+++ js/common.js
@@ -1,2 +1,13 @@
 // 配置axios基地址
 axios.defaults.baseURL = 'https://hmajax.itheima.net'
+
+// 抽取请提示函数
+function showToast(msg) {
+  const toastDom = document.querySelector('.my-toast')
+  // 实列化toast组件
+  const toast = new bootstrap.Toast(toastDom)
+  // 修改内容并显示
+  document.querySelector('.toast-body').innerText = msg
+  toast.show()
+}
+// showToast('你好吗？')
