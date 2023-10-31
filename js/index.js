5bd3bdae1e9a97c3618115b1c1906f1c1ac03c37
milibing <1528238450@qq.com>
2023-10-31 21:05:15 +0800

    首恶-登录状态失败

--- js/index.js
+++ js/index.js
@@ -10,23 +10,40 @@ registerLogout()
 // 首页-统计数据
 async function getData() {
   const token = localStorage.getItem('token')
-  // 调用接口后(登录成功之后才可以调用)
-  const res = await axios({
-    url: '/dashboard',
-    // 请求头中携带token
-    // 不携带token,直接报错
-    headers: {
-      Authorization: token
-    }
-  })
-  // console.log(res)
-  const overview = res.data.data.overview
-  console.log(overview)
+  try {
+    // 调用接口后(登录成功之后才可以调用)
+    const res = await axios({
+      url: '/dashboard',
+      // 请求头中携带token
+      // 不携带token,直接报错
+      headers: {
+        Authorization: token
+      }
+    })
+    // console.log(res)
+    const overview = res.data.data.overview
+    // console.log(overview)
+  
+    // 渲染数据
+    Object.keys(overview).forEach(key => {
+      document.querySelector(`.${key}`).innerHTML = overview[key]
+    })
+  } catch (error) {
+    // 首页-登录状态过期
+    // 判断token失效 (状态码401) :token过期,token被篡改
+    if (error.response.status === 401) {
+      // 删除缓存并提示用户
+      localStorage.removeItem('token')
+      localStorage.removeItem('username')
+      // 使用普通用户可以理解的方式提示他们
+      showToast('请重新登录')
 
-  // 渲染数据
-  Object.keys(overview).forEach(key => {
-    document.querySelector(`.${key}`).innerHTML = overview[key]
-  })
+      // 返回登录页
+      setTimeout(() => {
+        location.href = 'login.html'
+      }, 1500)
+    }
+  }
 }
 
 getData()
