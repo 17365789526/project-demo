904ea375d2399458552c4b77b3676bf84c71669c
milibing <1528238450@qq.com>
2023-11-02 21:33:07 +0800

    信息管理-学员信息渲染

--- js/student.js
+++ js/student.js
@@ -6,3 +6,34 @@ renderUsername()
 
 // 整合函数-退出登录逻辑
 registerLogout()
+
+// 抽取函数-获取学员数据
+async function getData() {
+  const res = await axios.get('/students')
+  // console.log(res)
+  // 渲染数据
+  const html = res.data.map(v => {
+    const { name, age, gender, group, hope_salary, salary, province, city, area, id } =v
+    return `
+      <tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${gender === 0 ? '男' : '女'}</td>
+        <td>第${group}组</td>
+        <td>${hope_salary}</td>
+        <td>${salary}</td>
+        <td>${province + city + area}</td>
+        <td data-id="${id}">
+          <a href="javascript:;" class="text-success mr-3"><i class="bi bi-pen"></i></a>
+          <a href="javascript:;" class="text-danger"><i class="bi bi-trash"></i></a>
+        </td>
+      </tr>
+    `
+  }).join('')
+
+  document.querySelector('.list').innerHTML = html
+
+  document.querySelector('.total').innerText = res.data.length
+}
+
+getData()
