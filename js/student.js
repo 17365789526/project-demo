bb22b520f717a413b1d88a0af1cf42ca777fb6e4
milibing <1528238450@qq.com>
2023-11-02 22:15:01 +0800

    信息管理-新增学员

--- js/student.js
+++ js/student.js
@@ -37,3 +37,89 @@ async function getData() {
 }
 
 getData()
+
+// 显示弹框
+const modalDom = document.querySelector('#modal')
+const modal = new bootstrap.Modal(modalDom)
+
+document.querySelector('#openModal').addEventListener('click', () => {
+  modal.show()
+})
+
+// 省市区联动
+const proSelect = document.querySelector('[name=province]')
+const citySelect = document.querySelector('[name=city]')
+const areaSelect = document.querySelector('[name=area]')
+
+async function initSelect() {
+  // 省数据获取+渲染
+  const proRes = await axios.get('/api/province')
+  // console.log(proRes)
+  const proHtml = proRes.list.map(v => {
+    return `<option value="${v}">${v}</option>`
+  }).join('')
+  proSelect.innerHTML = `<option value="">--省份--</option>${proHtml}`
+
+  // 市数据获取+渲染
+  proSelect.addEventListener('change', async () => {
+    const cityRes = await axios.get('/api/city', {
+      params: {
+        pname: proSelect.value
+      }
+    })
+    const cityHtml = cityRes.list.map(v => {
+      return `<option value="${v}">${v}</option>`
+    }).join('')
+    citySelect.innerHTML = `<option value="">--城市--</option>${cityHtml}`
+
+    // 一会需要清空地区标签的内容，否则会有Bug(停留在上一次的状态)
+    areaSelect.innerHTML = `<option value="">--地区--</option>`
+  })
+
+  // 区数据获取+渲染
+  citySelect.addEventListener('change', async () => {
+    const areaRes = await axios.get('/api/area', {
+      params: {
+        pname: proSelect.value,
+        cname: citySelect.value
+      }
+    })
+    const areaHtml = areaRes.list.map(v => {
+      return `<option value="${v}">${v}</option>`
+    }).join('')
+    areaSelect.innerHTML = `<option value="">--城市--</option>${areaHtml}`
+  })
+}
+
+initSelect()
+
+// 数据薪增
+document.querySelector('#submit').addEventListener('click', () => {
+  // 调用薪资学员函数
+  addStudent()
+})
+// 函数抽取-新增学员
+async function addStudent() {
+  // 数据收集+转换+提交
+  const form = document.querySelector('#form')
+  const data = serialize(form, { hash: true, empty: true })
+
+  data.age = +data.age
+  data.gender = +data.gender
+  data.hope_salary = +data.hope_salary
+  data.salary = +data.salary
+  data.group = +data.group
+  console.log(data)
+
+  try {
+    // 新增成功
+    const res = await axios.post('/students', data)
+    showToast(res.message)
+    getData()
+  } catch (error) {
+    // 新增失败
+    console.dir(error)
+    showToast(error.response.data.message)
+  }
+  modal.hide()
+}
